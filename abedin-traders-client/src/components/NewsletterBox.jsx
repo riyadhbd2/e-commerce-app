bfc97979c4802753ee320c3e4b2c8a0fb9fba4ac
riyadhbd2 <riyadhbd2@gmail.com>
2024-10-23 08:56:31 +0200

    footer added

---
+++ abedin-traders-client/src/components/NewsletterBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+
+    const onSubmitHandler = (e) =>{
+        e.preventDefault();
+        
+
+    }
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Subcribe now and & get 20% off</p>
+        <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet.</p>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+            <input className='w-full sm:flex-1 outline-none' type="email" name="" id="" placeholder='Enter your email' required />
+            <button className='bg-black text-white text-xs px-10 py-4' type="submit">SUBSCRIBE</button>
+        </form>
+    </div>
+  )
+}
+
+export default NewsletterBox
